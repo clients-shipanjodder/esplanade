62146ee07cdc26518ad5605014b1b4a3126803d8
Shipan Jodder <158731879+clients-shipanjodder@users.noreply.github.com>
2024-09-17 00:34:48 +0600

    up

--- js/modula-albums-pro-check.js
+++
@@ -1,1 +0,0 @@
-(function($){$(document).ready(function(){$('html body').on('click','#modula_albums_pro_check button.notice-dismiss',function(){var expires=new Date();expires.setTime(expires.getTime()+(7*24*60*60*1000));document.cookie='modula_albums_pro_check=true;expires='+expires.toUTCString();});});})(jQuery);
